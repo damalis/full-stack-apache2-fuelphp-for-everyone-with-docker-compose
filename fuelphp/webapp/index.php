aae2cea5d6a031e23a65d9a4cd732d2df8952b29
Erdal ALTIN <erdalaltin@gmail.com>
2022-10-20 13:18:07 +0300

    Delete index.php

--- fuelphp/webapp/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('APPPATH', realpath( __DIR__ . '/fuel/app/').DIRECTORY_SEPARATOR );
-define('PKGPATH', realpath( __DIR__ . '/fuel/packages/').DIRECTORY_SEPARATOR );
-define('COREPATH', realpath( __DIR__ . '/fuel/core/').DIRECTORY_SEPARATOR );
