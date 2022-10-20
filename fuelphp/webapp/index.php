2e5e255507bff3d1d79574ae42b6b88271115d70
Erdal ALTIN <erdalaltin@gmail.com>
2022-10-20 11:17:50 +0300

    Initial commit

---
+++ fuelphp/webapp/index.php
@@ -0,0 +1,5 @@
+<?php
+
+define('APPPATH', realpath( __DIR__ . '/fuel/app/').DIRECTORY_SEPARATOR );
+define('PKGPATH', realpath( __DIR__ . '/fuel/packages/').DIRECTORY_SEPARATOR );
+define('COREPATH', realpath( __DIR__ . '/fuel/core/').DIRECTORY_SEPARATOR );
